aacee7829704a3bf4568190469b323bdac762a30
이두영 <leeduyoung20@naver.com>
2016-04-02 23:49:26 +0900

    * Express에 view engine인 Jade 모듈 추가

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
+// node.js용 웹프레임워크 express에 jade라고하는 node.js용 view engine 추가 (연결)
+app.set('view engine', 'jade');
+
+// jade 템플릿이 들어있을 경로 - 생략 하더라도 디폴트값으로 설정됨. 아래와 동일.
+app.set('views', './views')
+
 //public directory를 정적인 페이지를 두는데 사용한다.
 app.use(express.static('public'));
+
+// 웹상에서 html코드 이쁘게 보이게 하기.
+app.locals.pretty = true;
+
+app.get('/template', function(req, res){
+  res.render('temp', {time:new Date(), _title:'Jade', _subtitle:'Jade를 배워봅니다.'});
+  //res.send();
+})
 
 app.get('/route', function(req, res){
   res.send('Hello Router, <img src="/route.png">');
