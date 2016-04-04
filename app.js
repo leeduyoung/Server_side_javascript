a657d043ab757b90c76aeeb4390dda5a8d4e10b7
이두영 <leeduyoung20@naver.com>
2016-04-05 01:04:26 +0900

    * 프로젝트에 npm을 사용하여 body-parser추가 - post요청을 받아들이기 위함.

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); //post 요청 처리시 사용.
 
 // node.js용 웹프레임워크 express에 jade라고하는 node.js용 view engine 추가 (연결)
 app.set('view engine', 'jade');
 
 // jade 템플릿이 들어있을 경로 - 생략 하더라도 디폴트값으로 설정됨. 아래와 동일.
-app.set('views', './views')
+app.set('views', './views');
 
 //public directory를 정적인 페이지를 두는데 사용한다.
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded (우리 프로젝트에 붙이는것. Express에 붙이는것임)
+// post방식 사용하려면 추가해줘야함
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // 웹상에서 html코드 이쁘게 보이게 하기.
 app.locals.pretty = true;
+
+app.post('/form_receiver', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title+', '+description);
+})
+
+app.get('/formpost', function(req, res){
+  res.render('formpost');
+});
+
+app.get('/form_receiver', function(req, res){
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title+', '+description);
+})
+
+app.get('/form', function(req, res){
+  res.render('form');
+});
+
+// sementic url 사용하기
+app.get('/topic/:id', function(req, res){
+  var topics = ['Javascript', 'Nodejs', 'Express', 'Jade'];
+  var lis = '';
+  var time = new Date();
+  for (var i = 0; i < topics.length; i++) {
+    //lis = lis + '<li>'+topics[i]+'</li>';
+    lis += '<a href="/topic/'+i+'">'+topics[i]+'</a><br>'
+  }
+  var output = `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    ${lis}<br>
+    ${topics[req.params.id]}<br>
+    ${time}
+  </body>
+  </html>
+`;
+  res.send(output);
+});
 
 app.get('/topic', function(req, res){
   var topics = ['Javascript', 'Nodejs', 'Express', 'Jade'];
